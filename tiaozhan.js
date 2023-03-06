9ac9ca36bd319510433efbb0962755cb197caeeb
zhugui00 <63114148+zhugui00@users.noreply.github.com>
2023-03-07 07:29:39 +0800

    Update tiaozhan.js

--- tiaozhan.js
+++ tiaozhan.js
@@ -1,3 +1,4 @@
 var body = $request.body;
-body = body.replace(/"rightQuestionNum"\s*:\s*\d+/g, "\"rightQuestionNum\": 10");
+var newNum = Math.floor(Math.random() * 6) + 5;
+body = body.replace(/"rightQuestionNum"\s*:\s*\d+/g, "\"rightQuestionNum\": + newNum);
 $done({body});
