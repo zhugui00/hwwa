af459f9ce8d2d7a1dc62eb39ab8121977975f21e
zhugui00 <63114148+zhugui00@users.noreply.github.com>
2023-03-13 11:46:34 +0800

    Update leng.js

--- leng.js
+++ leng.js
@@ -1,2 +1,3 @@
-// 修改请求头
-$done({url: $request.url.replace("udid=00008110-001A118C0A79801E", "udid=00008110-000A09A00A91801E")});
+let url = $request.url;
+url = url.replace(/udid=[^&]*/, "udid=00008110-001A118C0A79801E");
+$done({url});
