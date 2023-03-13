262efd70a0a13776801cea9c4c7b98c33397b516
zhugui00 <63114148+zhugui00@users.noreply.github.com>
2023-03-13 12:01:02 +0800

    Update leng.js

--- leng.js
+++ leng.js
@@ -1,10 +1,3 @@
 let url = $request.url;
-const regex = /(\?|&)udid=([^&]*)/;
-const match = regex.exec(url);
-if (match) {
-  const originalValue = match[2];
-  url = url.replace(regex, `$1udid=00008110-001A118C0A79801E`);
-} else {
-  url += (url.includes('?') ? '&' : '?') + 'udid=00008110-001A118C0A79801E';
-}
+url = url.replace(/(\?|&)udid=[^&]*/, '$1udid=00008110-001A118C0A79801E');
 $done({ url });
