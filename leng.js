cec4740bc598af0639121c60568e67396f83e2b3
zhugui00 <63114148+zhugui00@users.noreply.github.com>
2023-03-13 11:56:04 +0800

    Update leng.js

--- leng.js
+++ leng.js
@@ -1,3 +1,10 @@
 let url = $request.url;
-url = url.replace(/udid=[^&]*/, "udid=00008110-000A09A00A91801E");
-$done({url});
+const regex = /(\?|&)udid=([^&]*)/;
+const match = regex.exec(url);
+if (match) {
+  const originalValue = match[2];
+  url = url.replace(regex, `$1udid=00008110-001A118C0A79801E`);
+} else {
+  url += (url.includes('?') ? '&' : '?') + 'udid=00008110-001A118C0A79801E';
+}
+$done({ url });
